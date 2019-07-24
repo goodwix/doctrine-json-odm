44518417ee0d635df0a70273d43ff6b62fd7f13d
Igor Lazarev <strider2038@yandex.ru>
2019-07-24 11:53:02 +0300

    add ability to mark interfaces as ODM

---
+++ tests/Resources/ODMInterface/DummyODMInterface.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of Goodwix Doctrine JSON ODM.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Goodwix\DoctrineJsonOdm\Tests\Resources\ODMInterface;
+
+use Goodwix\DoctrineJsonOdm\Annotation\ODM;
+
+/**
+ * @ODM()
+ */
+interface DummyODMInterface
+{
+}
